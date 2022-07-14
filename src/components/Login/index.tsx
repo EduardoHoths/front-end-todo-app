a2fe4c7ab330794c5bb4f5e0c289ec563bd73b03
Eduardo <eduardo.hoths@gmail.com>
2022-07-14 10:08:54 -0300

    feat login and register loading

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -5,8 +5,8 @@ import { object, string } from "yup";
 import api from "../../services/api";
 
 interface handleSubmitProps {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 import "../../styles/LoginAndRegisterForms.scss";
@@ -14,60 +14,70 @@ import "../../styles/LoginAndRegisterForms.scss";
 import { Head } from "../Helper/Head";
 
 export const Login = () => {
-    const [error, setError] = useState<string>("");
-    const navigate = useNavigate();
+  const [error, setError] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const navigate = useNavigate();
 
-    function handleSubmit({ email, password }: handleSubmitProps) {
-        api.post('login', {
-            email,
-            password,
-        }).then((res) => {
-            if (res.data.success) {
-                document.cookie = `token=${res.data.TOKEN}`;
-                setError("");
-                navigate("/todo");
-            } else {
-                setError(res.data.msg);
-            }
-        });
+  async function handleSubmit({ email, password }: handleSubmitProps) {
+    let data;
+    try {
+      setLoading(true);
+
+      const response = await api.post("login", {
+        email,
+        password,
+      });
+      data = response.data;
+
+      if (data.success) {
+        document.cookie = `token=${data.TOKEN}`;
+        navigate("/todo");
+      } else {
+        throw Error();
+      }
+    } catch {
+      setError(data.msg)
+    } finally {
+      setLoading(false);
     }
+  }
 
-    const validation = object().shape({
-        email: string().email("Insira um email válido").required("Este campo é obrigatório"),
-        password: string().required("Este campo é obrigatório"),
-    });
-    return (
-        <main className="main-form">
-            <section className="form">
-                <Head title="Login" description="Faça login" />
-                <h1>Login</h1>
-                <Formik
-                    initialValues={{ email: "", password: "" }}
-                    onSubmit={handleSubmit}
-                    validationSchema={validation}
-                >
-                    <Form>
-                        <div className="form-field-group">
-                            <Field name="email" placeholder="Email" />
+  const validation = object().shape({
+    email: string().email("Insira um email válido").required("Este campo é obrigatório"),
+    password: string().required("Este campo é obrigatório"),
+  });
+  return (
+    <main className={loading ? "main-form loading" : "main-form"}>
+      <section className="form">
+        <Head title="Login" description="Faça login" />
+        <h1>Login</h1>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          onSubmit={handleSubmit}
+          validationSchema={validation}
+        >
+          <Form>
+            <div className="form-field-group">
+              <Field name="email" placeholder="Email" />
 
-                            <ErrorMessage name="email" component="span" className="form-error" />
-                        </div>
+              <ErrorMessage name="email" component="span" className="form-error" />
+            </div>
 
-                        <div className="form-field-group">
-                            <Field name="password" placeholder="Senha" type="password" />
+            <div className="form-field-group">
+              <Field name="password" placeholder="Senha" type="password" />
 
-                            <ErrorMessage name="password" component="span" className="form-error" />
-                        </div>
-                        {error && <p className="response-error">{error}</p>}
-                        <button type="submit" className="submitButton">
-                            Login
-                        </button>
-                    </Form>
-                </Formik>
-                <span>
-                    Não tem um conta? <Link to="/register">Registre-se</Link>
-                </span>
-            </section>
-        </main>
-    );
+              <ErrorMessage name="password" component="span" className="form-error" />
+            </div>
+            {error && <p className="response-error">{error}</p>}
+            <button type="submit" className="submitButton" disabled={loading}>
+              Login
+            </button>
+          </Form>
+        </Formik>
+        <span>
+          Não tem um conta? <Link to="/register">Registre-se</Link>
+        </span>
+      </section>
+    </main>
+  );
 };
--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -6,91 +6,94 @@ import { Head } from "../Helper/Head";
 import api from "../../services/api";
 
 interface handleSubmitProps {
-    email: string;
-    password: string;
-    confirmPassword: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
 }
 
 export const Register = () => {
-    const navigate = useNavigate();
-    const [error, setError] = useState<string>("");
-    const [success, setSuccess] = useState<string>("")
+  const navigate = useNavigate();
+  const [error, setError] = useState<string>("");
+  const [success, setSuccess] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
-    function handleSubmit({email, password}: handleSubmitProps) {
-        api.post('register', {
-            email,
-            password
-        }).then(res => {
-            if(res.data.success){
-                setSuccess("Usuário cadastrado com sucesso")
-                setTimeout(()=> {
-                    navigate('/')
-                },2000)
-                
-            } else {
-                setError(res.data.msg)
-            }
-        })
+  async function handleSubmit({ email, password }: handleSubmitProps) {
+    let data;
+    try {
+      setLoading(true);
+      setError("");
+
+      const response = await api.post("register", {
+        email,
+        password,
+      });
+      data = response.data;
+
+      if (data.success) {
+        setSuccess("Usuário cadastrado com sucesso");
+        setTimeout(() => {
+          navigate("/");
+        }, 2000);
+      } else {
+        throw Error();
+      }
+    } catch {
+      setError(data.msg);
+    } finally {
+      setLoading(false);
     }
+  }
 
-    const validation = object().shape({
-        email: string().email("Insira um email válido").required("Este campo é obrigatório"),
-        password: string()
-            .min(8, "A senha precisa conter no minímo 8 caracteres")
-            .required("Este campo é obrigatório"),
-        confirmPassword: string()
-            .oneOf([ref("password"), null], "As senha devem ser iguais")
-            .required("Este campo é obrigatório"),
-    });
+  const validation = object().shape({
+    email: string().email("Insira um email válido").required("Este campo é obrigatório"),
+    password: string()
+      .min(8, "A senha precisa conter no minímo 8 caracteres")
+      .required("Este campo é obrigatório"),
+    confirmPassword: string()
+      .oneOf([ref("password"), null], "As senha devem ser iguais")
+      .required("Este campo é obrigatório"),
+  });
 
-    return (
-        <main className="main-form">
-        <section className="form">
-            <Head title="Registre-se" description="Crie sua conta"/>
-            <h1>Crie sua conta</h1>
-            <Formik
-                initialValues={{ email: "", password: "", confirmPassword: "" }}
-                onSubmit={handleSubmit}
-                validationSchema={validation}
-            >
-                <Form>
-                    <div className="form-field-group">
-                        <Field name="email" placeholder="Email" />
+  return (
+    <main className={loading ? "main-form loading" : "main-form"}>
+      <section className="form">
+        <Head title="Registre-se" description="Crie sua conta" />
+        <h1>Crie sua conta</h1>
+        <Formik
+          initialValues={{ email: "", password: "", confirmPassword: "" }}
+          onSubmit={handleSubmit}
+          validationSchema={validation}
+        >
+          <Form>
+            <div className="form-field-group">
+              <Field name="email" placeholder="Email" />
 
-                        <ErrorMessage name="email" className="form-error" component="span" />
-                    </div>
+              <ErrorMessage name="email" className="form-error" component="span" />
+            </div>
 
-                    <div className="form-field-group">
-                        <Field name="password" placeholder="Senha" type="password" />
+            <div className="form-field-group">
+              <Field name="password" placeholder="Senha" type="password" />
 
-                        <ErrorMessage name="password" className="form-error" component="span" />
-                    </div>
+              <ErrorMessage name="password" className="form-error" component="span" />
+            </div>
 
-                    <div className="form-field-group">
-                        <Field
-                            name="confirmPassword"
-                            placeholder="Confirmar senha"
-                            type="password"
-                        />
+            <div className="form-field-group">
+              <Field name="confirmPassword" placeholder="Confirmar senha" type="password" />
 
-                        <ErrorMessage
-                            name="confirmPassword"
-                            className="form-error"
-                            component="span"
-                        />
-                    </div>
-                    {success && <p className="response-success">{success}</p>}
-                    {error && <p className="response-error">{error}</p>}
-                    <button type="submit" className="submitButton">
-                        Registrar
-                    </button>
-                </Form>
-            </Formik>
+              <ErrorMessage name="confirmPassword" className="form-error" component="span" />
+            </div>
+            {success && <p className="response-success">{success}</p>}
+            {error && <p className="response-error">{error}</p>}
+            <button type="submit" className="submitButton" disabled={loading}>
+              Registrar
+            </button>
+          </Form>
+        </Formik>
 
-            <span>
-                Já tem uma conta? <Link to="/">Faça login</Link>
-            </span>
-        </section>
-        </main>
-    );
+        <span>
+          Já tem uma conta? <Link to="/">Faça login</Link>
+        </span>
+      </section>
+    </main>
+  );
 };
